f9aabdc7a3643b27d118247acd7383edf00f7a6e
Bianca Ikari <biaa.freeitas@gmail.com>
2022-09-28 22:44:38 +0100

    Create

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 require("dotenv").config();
 const express = require('express')
-const app = express();
 const connectToDb = require("./database/db")
+const path = require("path");
+const Musica = require("./model/Musicas")
 
+const app = express();
 const port = process.env.PORT || 3000;
+
+//O EJS permite utilizar JS dentro do HTML
+app.set("view engine", "ejs")
+
+//Arquivos estáticos ficam na pasta indicada
+app.use(express.static(path.join(__dirname, "public")));
+
+//Intersecção de dados de formulário html 
+app.use(express.urlencoded());
+
 
 connectToDb();
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.render("index");
+});
+
+app.get("/admin", (req, res) => {
+    res.render("admin");
+});
+
+app.post("/create", async (req, res) => {
+    const music = req.body;
+    await Musica.create(music);
+    res.redirect("/")
 });
 
 app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`)
